7605e8b360c31d38e23851324123423617e901a9
indiomedeiros <bboyindio@gmail.com>
2020-11-26 12:36:17 -0300

    Ajustes na requisição, agora tem todos os pokemons com todos os atribuos que precisamos mapeados

--- pokedex/src/Components/global/GlobalState.js
+++ pokedex/src/Components/global/GlobalState.js
@@ -1,38 +1,54 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState} from "react";
 import axios from "axios";
-import { BASE_URL } from "../constants/urls";
+
 import GlobalStateContext from "./GlobalStateContext";
 
 const GlobalState = (props) => {
   //pokemonList é a lista de 20 pokemons 
-  //pokedex é a lista de pokemons da pokedex que sera usada depois
+  //pokedex é a lista de pokemons 
   const [pokemonList, setPokemonList] = useState([]);
-const [pokedex,setPokedex] = useState([ ])
 
-const getPokemons = ( ) =>  {
-  axios
-  .get('${BASE_URL} ')
-  .then((response) => {
-    // console.log(response);
-    setPokemonList(response.data.results);
-  })
-  .catch((error) => {
-    alert(error.messages);
-  });
-}
-//states tem o states,setters tem o setStates e o request a função
-const states = {pokemonList,pokedex}
-const setters = {setPokemonList,setPokedex}
-const request = {getPokemons}
+useEffect(() => {
+  getPokemons() 
+}, [])
 
+const getPokemons = () => {
+  //array estratégico para enviar uma única vez ao state
+  const listPokemon = [];
+  //numero de requisições de acordo com a quantidade de pokemnos
+  //que você desejar.
+  //cria um array de pokemons de acordo com a quantidade de repetições
+  for (let i = 1; i < 24; i++) {
+    axios
+    .get(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+      .then((response) => {
+        listPokemon[i - 1] = {
+          id: response.data.id,
+          name: response.data.name,
+          status: response.data.stats,
+          moves: response.data.moves,
+          types: response.data.types,
+          sprites: response.data.sprites,            
+        };
 
-//esquema de separação
-const data = {states,setters,request}
+        //condição para envio da lista
+        //se a largura estivar do tamanho que vc desejar, 
+        //ele vai passar para o state
+        if (listPokemon.length === 23) {
+          setPokemonList(listPokemon);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+};
 
   return (
     <div>
-      <GlobalStateContext.Provider value={data}>
+      <GlobalStateContext.Provider value={pokemonList}>
       {props.children}
+      {console.log(pokemonList)}
     </GlobalStateContext.Provider>
     </div>
   );
