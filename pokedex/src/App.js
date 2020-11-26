f973d9e4dcf37d2243fd626bfa554b564933e354
Michael Soto <michael_nickolas@hotmail.com>
2020-11-26 15:56:22 -0300

    Feito componente PokeCard,Home,adc Home no Router

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,11 +1,9 @@
 import React, { useContext } from "react";
 import Router from "./routes/Router";
 import GlobalState from './Components/global/GlobalState';
-
+import Home from "./Components/Home/Home";
 
 function App() {
-  
- 
   return (
     <div className="App">
 
--- pokedex/src/Components/Home/Home.js
+++ pokedex/src/Components/Home/Home.js
@@ -1,33 +1,49 @@
-import React, { useContext, useEffect } from 'react';
-import GlobalState from '../global/GlobalState';
+import React, { useContext } from 'react';
+// import GlobalState from '../global/GlobalState';
 import GlobalStateContext from '../global/GlobalStateContext';
+import { useHistory } from 'react-router-dom';
+import PokeCard from '../PokeCard/PokeCard'
 
-const {states,requests} = useContext(GlobalStateContext)
 
 import { GridContainer } from './styled';
 
 function Home  ()  {
-const {states,requests} = useContext(GlobalStateContext)
+  const pokemonList = useContext(GlobalStateContext)
 
+      //hook para manipular a troca de páginas
+      const history = useHistory()
 
-useEffect(( ) => {
-  requests.getPokemons( )
-},[ ])
+       //função para trocar de página
+    const goToPage = (path) => {
+      history.push (path)
+  }
 
+const funcaoAdicionar = ( )  => {
+console.log('fefe')
+}
 
+const funcaoDetalhes = ( )  => {
+  console.log('fgefse')
+  }
 
   return(
-    <div>
+   
+  
 <GridContainer>
+{console.log('werdw', pokemonList)}
   {/* pega o estado pokemonList e mapeia ele pra pegar o url */}
-{states.pokemonList && states.pokemonList.map((item) =>{
-  return <CardPokemon url={item.url} />
-})}
+  {pokemonList && pokemonList.map((item) =>{
+  return <PokeCard key={item.id} adicionar={funcaoAdicionar}  detalhes={funcaoDetalhes} img={item.sprites.front_default} /> 
+
+
+  }
+  )
+}
 
 </GridContainer>
 
       
-    </div>
+    
 
 
 
